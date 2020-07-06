4f8aad3f7cf8447bf76ef132948559e323127181
Neelabh Mam <neelabh.mam@ibm.com>
2020-07-06 22:42:37 +0530

    initial fpd

--- fpd.cpp
+++ fpd.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include <npl.hpp>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	
+  if (argc != 3)
+  {
+    std::cout << "usage : Agent <host> <port>\n";
+    std::cout << "usage : Agent 0.0.0.0 8081\n";
+    return 0;
+  }
+
+  auto host = std::string(argv[1]);
+
+  auto port = std::stoi(argv[2]);
+
+  auto ws = NPL::make_ws_server(
+    host, port, TLS::YES, 
+    [] (SPCProtocol c, const std::string& m) 
+    {
+      std::cout << "client : " << m << "\n";
+
+      c->SendProtocolMessage(
+        (uint8_t *)"server echo : hello", 
+        strlen("server echo : hello")
+      );
+    }
+  );
+
+  ws->StartServer();
+
+  getchar();
+
   return 0;
 }
